f7efb9fceee5a359f6de036151558669c77ebc3e
OllyDorvelus <ollydorvelus@gmail.com>
2019-02-23 19:48:23 -0500

    change url paths for api to plural

--- orders/api/urls.py
+++ orders/api/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
      path('new/', views.OrderCreateAPIView.as_view(), name='order-create-api'),
      path('request/', views.ServiceOrderListAPIView.as_view(), name='request-list-api'),
      path('cart/', views.CartDetailAPIView.as_view(), name='cart-detail-api'),
-     path('complaint/create/<entry_id>/', views.ComplaintCreateAPIView.as_view(), name='complaint-create-api'),
-     path('complaint/<pk>', views.ComplaintDetailAPIView.as_view(), name='complaint-detail-api'),
-     path('entry/<pk>/', views.EntryDetailAPIView.as_view(), name='entry-detail-api'),
-     path('entry/service/<pk>/', views.ServiceOwnerEntryDetailAPIView.as_view(), name='entry-service-owner-detail-api'),
-     path('entry/add/<service_id>/', views.AddEntryToCartAPIView.as_view(), name='add-entry-to-cart-api')
+     path('complaints/create/<entry_id>/', views.ComplaintCreateAPIView.as_view(), name='complaint-create-api'),
+     path('complaints/<pk>', views.ComplaintDetailAPIView.as_view(), name='complaint-detail-api'),
+     path('entries/<pk>/', views.EntryDetailAPIView.as_view(), name='entry-detail-api'),
+     path('entries/service/<pk>/', views.ServiceOwnerEntryDetailAPIView.as_view(), name='entry-service-owner-detail-api'),
+     path('entries/add/<service_id>/', views.AddEntryToCartAPIView.as_view(), name='add-entry-to-cart-api')
 ]
--- services/api/urls.py
+++ services/api/urls.py
@@ -6,8 +6,8 @@ app_name = 'services_api'
 urlpatterns = [
     path('', views.ServiceListAPIView.as_view(), name='service-list-api'),
     path('categories/', views.CategoryListAPIView.as_view(), name='category-list-api'),
-    path('category/create/', views.CategoryCreateAPIView.as_view(), name='category-create-api'),
-    path('category/<pk>/', views.CategoryDetailAPIView.as_view(), name='category-detail-api'),
+    path('categories/create/', views.CategoryCreateAPIView.as_view(), name='category-create-api'),
+    path('categories/<pk>/', views.CategoryDetailAPIView.as_view(), name='category-detail-api'),
     path('main-categories/', views.MainCategoryListAPIView.as_view(), name='main-category-list-api'),
     path('sub-categories/<pk>/', views.SubCategoryListAPIView.as_view(), name='sub-category-list-api'),
     path('<service_pk>/categories/', views.ServiceCategoryListAPIView.as_view(), name='service-category-list-api'),
@@ -16,7 +16,8 @@ urlpatterns = [
     path('create/', views.ServiceCreateAPIView.as_view(), name='service-create-api'),
     path('<pk>/', views.ServiceDetailAPIView.as_view(), name='service-detail-api'),
     path('<user__username>/services/', views.UserServiceListAPIView.as_view(), name='user-service-list-api'),
+    path('reviews/<pk>/', views.ReviewDetailAPIView.as_view(), name='review-detail-api'),
     path('<pk>/reviews/', views.ServiceReviewListAPIView.as_view(), name='service-reviews-list-api'),
-    path('<pk>/review/create/', views.ReviewCreateAPIView.as_view(), name='review-create-api'),
-    path('review/<pk>/', views.ReviewDetailAPIView.as_view(), name='review-detail-api'),
+    path('<pk>/reviews/create/', views.ReviewCreateAPIView.as_view(), name='review-create-api'),
+
 ]
