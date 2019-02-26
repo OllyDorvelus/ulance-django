e6e8195b067ccafac0a647a52afeaa21725cbf6f
OllyDorvelus <ollydorvelus@gmail.com>
2019-02-26 00:44:06 -0500

    set up for service listings based on category

--- services/urls.py
+++ services/urls.py
@@ -6,4 +6,5 @@ app_name = 'services'
 urlpatterns = [
     path('services/new/', views.ServiceCreateView.as_view(), name='service-create'),
     path('services/<pk>/', views.ServiceDetailView.as_view(), name='service-detail'),
+    path('services/categories/<name>/', views.ServiceCategoryListView.as_view(), name='service-category-list')
 ]
--- services/views.py
+++ services/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
-from django.forms import model_to_dict
-from django.http import JsonResponse
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.contrib.auth import authenticate, login, logout
-from django.views.generic import View, DetailView, ListView, FormView, TemplateView
+# from django.shortcuts import render
+# from django.forms import model_to_dict
+# from django.http import JsonResponse
+# from django.views.generic.edit import CreateView, UpdateView, DeleteView
+# from django.contrib.auth import authenticate, login, logout
+from django.views.generic import DetailView, ListView, TemplateView
 from django.contrib.auth import get_user_model
 from django.shortcuts import render, redirect, get_object_or_404
-from profiles.models import ProfileModel, SkillModel, LevelModel, CertificationModel, EducationModel, LinkModel
-from services.models import ServiceModel
-User = get_user_model()
+# from profiles.models import ProfileModel, SkillModel, LevelModel, CertificationModel, EducationModel, LinkModel
+from services.models import ServiceModel, CategoryModel
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+User = get_user_model()
+
 # Create your views here.
 
 
@@ -26,3 +28,13 @@ class ServiceDetailView(DetailView):
 class ServiceCreateView(LoginRequiredMixin, TemplateView):
     template_name = 'create_service.html'
     login_url = '/login/'
+
+
+class ServiceCategoryListView(DetailView):
+    template_name = 'service_category_list.html'
+    queryset = CategoryModel.objects.all()
+    context_object_name = 'category'
+
+    def get_object(self):
+        category = get_object_or_404(CategoryModel, name__iexact=self.kwargs.get('name'))
+        return category
